73e7fc189beafbae77e1793422789708c6012fd4
AffonsoH <affonso.pavan@escola.pr.gov.br>
2026-03-18 15:24:34 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+altura = float(input("Qual sua altura? "))
+peso = float(input("Qual seu peso? "))
+idade = int(input("Qual sua idade?"))
+atleta = input("Você pratica algum esporte ou faz exercícios físicos? ")
+imc = float(peso / (altura*altura))
+listadepesos = ["Abaixo do peso", "Peso normal", "Acima do peso", "Obesidade grau 1", "Obesidade grau 2", "Obesdidade grau 3"]
+if atleta == "Sim" or atleta == "sim" or atleta == "s":
+    if imc < 18.6:
+        print(listadepesos[0]);
+    elif imc < 30.0:
+        print(listadepesos[1]);
+    elif imc < 40:
+        print(listadepesos[2])
+elif imc < 18.6:
+    print(listadepesos[0]);
+elif imc < 25.0:
+    print(listadepesos[1]);
+elif imc < 30.0:
+    print(listadepesos[2]);
+elif imc < 35.0:
+    print(listadepesos[3]);
+elif imc < 40:
+    print(listadepesos[4]);
+elif imc >= 40:
+    print(listadepesos[5])
